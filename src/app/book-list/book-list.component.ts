f7be68c2ec783e1fd519372dd4bd9c8c79b97039
BlackOut <benlama.amine@gmail.com>
2020-08-22 17:02:41 +0200

    book list component was updated

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { BooksService } from '../services/books.service';
+import { Router } from '@angular/router';
+import { Book } from '../models/book.model';
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.scss']
 })
-export class BookListComponent implements OnInit {
+export class BookListComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  books: Book[];
+  booksSubscription: Subscription;
 
-  ngOnInit(): void {
+  constructor(private booksService: BooksService, private router: Router) {}
+  // souscrit au Subject du service et déclenche sa première émission
+  ngOnInit() {
+    this.booksSubscription = this.booksService.booksSubject.subscribe(
+      (books: Book[]) => {
+        this.books = books;
+      }
+    );
+    this.booksService.emitBooks();
+  }
+
+  // permet de naviguer vers  BookFormComponent  pour la création d'un nouveau livre. 
+  onNewBook() {
+    this.router.navigate(['/books', 'new']);
+  }
+
+  onDeleteBook(book: Book) {
+    this.booksService.removeBook(book);
+  }
+
+  // permet de naviguer vers SingleBookComponent pour visualiser un livre donnée
+  onViewBook(id: number) {
+    this.router.navigate(['/books', 'view', id]);
+  }
+  
+  ngOnDestroy() {
+    this.booksSubscription.unsubscribe();
   }
 
 }
